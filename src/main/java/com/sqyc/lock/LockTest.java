cdbb029f246871b939fbd10f5c8e517dfc118f49
yanyunpeng_yy <yyp_it_no92@sina.com>
2021-03-04 11:26:59 +0800

    adsf

--- src/main/java/com/sqyc/lock/LockTest.java
+++ src/main/java/com/sqyc/lock/LockTest.java
@@ -1,39 +1,43 @@
 package com.sqyc.lock;
 
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-public class LockTest {
-	public static void main(String[] args) {
-		new Lock() {
-			
-			@Override
-			public void unlock() {
-			}
-			
-			@Override
-			public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
-				return false;
-			}
-			
-			@Override
-			public boolean tryLock() {
-				return false;
-			}
-			
-			@Override
-			public Condition newCondition() {
-				return null;
-			}
-			
-			@Override
-			public void lockInterruptibly() throws InterruptedException {
-			}
-			
-			@Override
-			public void lock() {
-			}
-		};
-	}
+public class LockTest extends Thread {
+    public static void main(String[] args) {
+        // https://www.cnblogs.com/xrq730/p/4979021.html
+        // CountDownLatch、CyclicBarrier、Sempahore
+
+        ReentrantLock reentrantLock = new ReentrantLock();
+        LockTest t1 = new LockTest("TryLockTest1");
+        LockTest t2 = new LockTest("TryLockTest2");
+
+        t1.start();
+        t2.start();
+    }
+
+    public static ReentrantLock lock = new ReentrantLock();
+
+    public LockTest(String name) {
+        super(name);
+    }
+
+    @Override
+    public void run() {
+        try {
+            if (lock.tryLock(5, TimeUnit.SECONDS)) {
+                Thread.sleep(6000);
+            } else {
+                System.out.println(this.getName() + " get lock failed");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            if (lock.isHeldByCurrentThread()) {
+                System.out.println("lock.isHeldByCurrentThread: " + this.getName());
+                lock.unlock();
+            }
+        }
+    }
+
 }
